853ed1e8e4f1a9bb0b80588724b74265506aab0d
8coolguy <arnavchoudhury20@mittymonarch.com>
2022-01-30 21:39:29 -0800

    getting filters in pygame

--- pygametest.py
+++ pygametest.py
@@ -1,7 +1,7 @@
 import pygame
 import pygame.camera
 import sys
-
+from pygame import Color
 
 
 pygame.init()
@@ -13,6 +13,7 @@ webCam =pygame.camera.Camera(pygame.camera.list_cameras()[0],(X,Y))
 screen=pygame.display.set_mode((X,Y))
 pygame.display.set_caption("Web Cam Stream")
 
+print(type(screen))
 webCam.start()
 
 frame = webCam.get_image()
@@ -20,6 +21,7 @@ while True:
     screen.blit(frame,(X/2,Y/2))
     pygame.display.update()
     frame =webCam.get_image()
+    pygame.draw.rect(screen,Color(255,255,0,255),(397,21,0,230))
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             sys.exit()
